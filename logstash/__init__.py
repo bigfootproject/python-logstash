49054fc57afbcea0cf9ed208db8faf697cb72275
Daniele Venzano <venza@brownhat.org>
2014-09-03 09:34:57 +0200

    Fix integer port

--- logstash/__init__.py
+++ logstash/__init__.py
@@ -8,5 +8,5 @@ def swift_setup(conf, name, log_to_console, log_route, fmt, logger,
                 adapted_logger):
     log_server = open("/etc/swift/log_server.conf").read()
     ip, port = log_server.split("|")
-    my_handler = UDPLogstashHandler(ip, port, version=1)
+    my_handler = UDPLogstashHandler(ip, int(port), version=1)
     logger.addHandler(my_handler)
